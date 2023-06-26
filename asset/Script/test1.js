5b311a01282da0abcdfd12dfa2754e048cde5b0d
Chhit <nooberchhit@gmail.com>
2023-06-26 14:30:27 +0700

    add css

--- asset/Script/test1.js
+++ asset/Script/test1.js
@@ -1,8 +1,32 @@
+function createTransitionInput(state, alpha) {
+  const transitionInput = document.createElement('div');
+  transitionInput.classList.add('transition-input');
+
+  const alphaLabel = document.createElement('label');
+  alphaLabel.textContent = alpha;
+  transitionInput.appendChild(alphaLabel);
+
+  const stateLabel = document.createElement('label');
+  stateLabel.textContent = state;
+  transitionInput.appendChild(stateLabel);
+
+  const arrowLabel = document.createElement('label');
+  arrowLabel.textContent = ' ----> ';
+  transitionInput.appendChild(arrowLabel);
+
+  const destInput = document.createElement('input');
+  destInput.type = 'text';
+  transitionInput.appendChild(destInput);
+
+  return { transitionInput, destInput };
+}
+
 function runAutomaton() {
   const states = document.getElementById('state-input').value.split(' ');
   const alphabets = document.getElementById('alphabet-input').value.split(' ');
   const startState = document.getElementById('start-state-input').value;
   const acceptStates = document.getElementById('accept-states-input').value.split(' ');
+  
 
   const transitionContainer = document.getElementById('transition-container');
   transitionContainer.innerHTML = '';
@@ -11,24 +35,7 @@ function runAutomaton() {
 
   for (const state of states) {
     for (const alpha of alphabets) {
-      const transitionInput = document.createElement('div');
-      transitionInput.classList.add('transition-input');
-
-      const alphaLabel = document.createElement('label');
-      alphaLabel.textContent = alpha;
-      transitionInput.appendChild(alphaLabel);
-
-      const stateLabel = document.createElement('label');
-      stateLabel.textContent = state;
-      transitionInput.appendChild(stateLabel);
-
-      const arrowLabel = document.createElement('label');
-      arrowLabel.textContent = ' ----> ';
-      transitionInput.appendChild(arrowLabel);
-
-      const destInput = document.createElement('input');
-      destInput.type = 'text';
-      transitionInput.appendChild(destInput);
+      const { transitionInput, destInput } = createTransitionInput(state, alpha);
 
       transitionContainer.appendChild(transitionInput);
 
